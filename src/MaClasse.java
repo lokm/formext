bc0ad239b8537b227669992e5dc8927341837a79
lokm <vincent.olivan@gmail.com>
2017-10-17 17:25:10 +0200

    Affiche age v2

--- src/MaClasse.java
+++ src/MaClasse.java
@@ -5,8 +5,28 @@ public class MaClasse {
 	public static void main(String[] args) {
 		String tab[] = {"tu as "," ans"};
 		int age = 18;
+		
+		// affichage brut
 		System.out.println(tab[0] + age + tab[1]);
 		
+		// avec boucle for
+		for	(int i = 0; i < tab.length; i++) {
+			if	(tab[i]  == tab[tab.length-1]) {
+				System.out.print(age + tab[i]);
+			} else {
+				System.out.print(tab[i]);
+			}
+		}
+		System.out.print("\n");
+		
+		//avec boucle foreach
+		for	(String str : tab) {
+			if (str.equals(tab[tab.length-1])) {
+				System.out.print(age + str);
+			} else {
+				System.out.print(str);
+			}
+		}
 	}
 
 }
