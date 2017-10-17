cc7a812c6dbd7a1c5145bee50c4a57fcd57ee657
lokm <vincent.olivan@gmail.com>
2017-10-17 16:22:18 +0200

    Affiche age

--- src/MaClasse.java
+++ src/MaClasse.java
@@ -3,25 +3,9 @@ import java.util.Arrays;
 public class MaClasse {
 
 	public static void main(String[] args) {
-		String x = "Bonjour";
-		String y = "à tous";
-		String tab[] = {x," ",y};
-		
-		// Affiche structure du tableau
-		String structTab = Arrays.toString(tab);
-		System.out.println(structTab);
-		
-		// Parcours le tableau avec un for
-		for (int i = 0; i < tab.length; i++) {
-			System.out.print(tab[i]);			
-		}
-		
-		System.out.print("\n");
-		
-		// Parcours le tableau avec un foreach
-		for (String j : tab) {
-			System.out.print(j);
-		}
+		String tab[] = {"tu as "," ans"};
+		int age = 18;
+		System.out.println(tab[0] + age + tab[1]);
 		
 	}
 
